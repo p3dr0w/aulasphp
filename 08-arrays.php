79a205df9c2d73a59df847a9f7331fa8754476e2
p3dr0w <pedro.hsilva@outlook.com>
2018-10-18 16:11:08 -0300

    salvando aula 08

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,8 +1,26 @@
 <?php
 //Arrays
-$carros = array("BMW","Veloster", "Hilux");
-
+$carros = array(5=>"BMW","Veloster", "Hilux");
+$carros[] = "Fusion";
+$carros[100] = "Classe A";
 print_r($carros);
 echo "<hr>";
-echo($carros[0]);
+echo($carros[6]);
+
+echo "<hr>";
+
+$motos = array();
+
+$motos[1] = ("Indian");
+$motos[] = "Caloi";
+$motos[] = "Harley";
+$motos[10] = "Bugatti";
+print_r($motos);
+echo "<br>";
+echo $motos[3];
+
+echo "<hr>";
+
+$clientes = array("João","Maria","Antônia");
+print_r($clientes);
 ?>
