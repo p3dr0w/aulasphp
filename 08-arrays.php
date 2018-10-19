84cdc0a057f827294dcc139a0870e5be3a281a7d
p3dr0w <pedro.hsilva@outlook.com>
2018-10-19 12:07:06 -0300

    08 - arrays

--- 08-arrays.php
+++ 08-arrays.php
@@ -23,4 +23,46 @@ echo "<hr>";
 
 $clientes = array("João","Maria","Antônia");
 print_r($clientes);
+echo "<hr>";
+
+// count
+
+echo count($clientes)." / ";
+// ou
+$totalClientes = count($clientes);
+echo $totalClientes;
+
+echo "<hr>";
+
+foreach ($carros as $valor) {
+ echo $valor."<br>";
+}
+
+echo "<hr>";
+
+// arrays associativos
+
+$pessoas = array("nome" => "Pedro", "idade" => 23, "altura" => 1.70);
+$pessoas["cidade"] = "Itaoca";
+echo $pessoas["nome"];
+echo "<hr>";
+print_r($pessoas);
+echo "<hr>";
+foreach ($pessoas as $indice => $valor) {
+  echo $indice.":".$valor."<br>";
+}
+echo "<hr>";
+// arrays multidimensionais
+$times = array(
+  "cariocas" => array("campeão"=>"vasco","vice"=>"flamengo", "terceiro"=>"botafogo"),
+  "paulistas"=> array("santos", "são paulo", "palmeiras"),
+  "baianos" => array("bahia", "vitoria","itabuna")
+);
+
+echo $times["paulistas"][0];
+echo "<hr>";
+foreach ($times["cariocas"] as $key => $value) {
+  echo $key.":".$value."<br>";
+}
+
 ?>
